e065d58a37ad016648f856ca9687b9cdaf2ff9f3
Johnathon Wood <jaywood128@gmail.com>
2018-09-18 18:43:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ function placeOrder(cardNumber) {
     } else {
      var sumCost = total()
      setCart([])
-     return `Your total cost is $${sumCost}, which will be charged to the card card ${cardNumber}.`
+     return `Your total cost is $${sumCost}, which will be charged to the card ${cardNumber}.`
 
    }
 }
